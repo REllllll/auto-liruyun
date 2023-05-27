1325e3f637ae606e02fee5ab2022f7a40f7a0b0a
Sytles <shonmorgan669@gmail.com>
2023-05-27 23:03:07 +0800

    feat: use selenium to do auto

--- liruyun auto.py
+++ liruyun auto.py
@@ -1,72 +1,71 @@
-import pyautogui
-from PIL import ImageGrab,ImageColor,Image
-import abc
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-class Unit():
+account = input("请输入用户名：")
+password = input("请输入密码：")
 
-    def __init__(self,position,type) -> None:
-        self.position = position
-        self.type = type
-        pass
-    
-    def click(self):
-        pyautogui.click(self.position)
-        pass
+browser = webdriver.Chrome()
+browser.get("https://moodle.scnu.edu.cn/")
 
-    def move(self):
-        pyautogui.moveTo(self.position)
-        pass
+browser.find_element(By.CLASS_NAME, "forgotpass").click()
+time.sleep(1)
+browser.find_element(By.ID, "ssobtn").click()
 
+time.sleep(1)
 
+# input username in the box
+browser.find_element(By.ID, "account").send_keys(account)
+# input password in the box
+browser.find_element(By.ID, "password").send_keys(password)
+# click the button
+browser.find_element(By.CLASS_NAME,
+                     "btn.btn-primary.btn-block.auth-login-btn").click()
+time.sleep(1)
+# stop the browser for 5 seconds
 
-def get_unit(screen : Image.Image):
-    unit_list = []
-    location_list = pyautogui.locateAll('liru_class_video_button.jpg',screen,confidence=0.9)
-    for location in location_list:
-        unit_list.append(Unit(location,'unit'))
-    return unit_list
+browser.find_element(By.CLASS_NAME, "float-right.login-check-comfirm").click()
+time.sleep(1)
 
-def play_video():
-    pyautogui.sleep(3)
-    #move to the position of the scroll bar
-    pyautogui.moveTo(pyautogui.locateOnScreen('position_scroll.jpg'))
-    #scroll to the bottom
-    pyautogui.scroll(-1000)
-    pyautogui.click(pyautogui.locateOnScreen('play_button.jpg'))
-    pass
+browser.find_element(By.ID, "expandable_branch_20_14426").click()
+time.sleep(1)
+chinese_group_list = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十"]
+for a in range(10):
 
-def wait_for_video():
-    while True:
-        pyautogui.sleep(1)
-        position = pyautogui.locateOnScreen('play_button.jpg')
-        if position == None:
-            continue
-        else:
-            pyautogui.sleep(1)
+    group_unit = browser.find_elements(By.CLASS_NAME, "item-content-wrap")
+    for group in group_unit:
+        if f"第{chinese_group_list[a]}讲" in group.text:
+            group.click()
             break
-    pass
-
-
-def main():
-    pyautogui.sleep(5)
-    screen = ImageGrab.grab()
-    unit_list = get_unit(screen)
-    for unit in unit_list:
+    a += 6
+    time.sleep(1)
+    unit_list = browser.find_elements(By.CLASS_NAME, "item-content-wrap")
+    current_unit_num = 0
+    # select the unit by the text which includes the keyword "【视频】"
+    for i in range(len(unit_list)):
+        unit_list = browser.find_elements(By.CLASS_NAME, "item-content-wrap")
+        video_list = []
+        for unit in unit_list:
+            if "【视频】" in unit.text:
+                video_list.append(unit)
+        unit = video_list[current_unit_num]
         unit.click()
-        play_video()
-        wait_for_video()
-    unit.move()
-    pyautogui.scroll(-1000)
-    screen = ImageGrab.grab()
-    unit_list = get_unit(screen)
-    for unit in unit_list:
-        pyautogui.scroll(-1000)
-        unit.click()
-        play_video()
-        wait_for_video()
-        unit.move()
-    pass
-
-if __name__ == '__main__':
-    main()
-    
+        time.sleep(5)
+        # find the video
+        browser.switch_to.frame("h5player")
+        frame = browser.find_element(
+            By.CLASS_NAME, "h5p-iframe.h5p-initialized").get_attribute("id")
+        browser.switch_to.frame(frame)
+        browser.find_element(By.CLASS_NAME, "h5p-splash-main-outer").click()
+        print(f"开始播放第{a-5}讲第{i+1}个视频，还剩{len(video_list)-i-1}个视频未播放")
+        time.sleep(1)
+        while True:
+            try:
+                browser.find_element(By.CLASS_NAME,
+                                     "h5p-control.h5p-play.h5p-pause")
+                break
+            except Exception:
+                pass
+        browser.switch_to.default_content()
+        print(f"第{a-5}讲第{i+1}个视频播放完毕")
+        current_unit_num += 1
